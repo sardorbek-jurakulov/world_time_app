3b98d1df1eb57f0a4aee7e8c531b6d1707781c1b
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-10-28 17:30:57 +0500

    WIP

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -36,8 +36,16 @@ class _HomeState extends State<Home> {
                   onPressed: () {
                     Navigator.pushNamed(context, '/location');
                   },
-                  icon: Icon(Icons.edit_location),
-                  label: Text('Edit location'),
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[300],
+                  ),
+                  label: Text(
+                    'Edit location',
+                    style: TextStyle(
+                      color: Colors.grey[300],
+                    ),
+                  ),
                 ),
                 SizedBox(height: 20.0),
                 Row(
@@ -48,6 +56,7 @@ class _HomeState extends State<Home> {
                       style: TextStyle(
                         fontSize: 28.0,
                         letterSpacing: 2.0,
+                        color: Colors.grey[300],
                       ),
                     ),
                   ],
@@ -58,6 +67,7 @@ class _HomeState extends State<Home> {
                   style: TextStyle(
                     fontSize: 28.0,
                     letterSpacing: 2.0,
+                    color: Colors.grey[300],
                   ),
                 )
               ],
