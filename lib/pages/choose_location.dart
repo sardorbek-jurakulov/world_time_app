7b570fd778a4db38e4aed64a50a80f58143919f1
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-10-28 19:38:18 +0500

    WIP

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -21,6 +21,17 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
   ];
 
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +50,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
             child: Card(
                 child: ListTile(
               onTap: () {
-                print(locations[index].location);
+                updateTime(index);
               },
               title: Text(locations[index].location),
               leading: CircleAvatar(
